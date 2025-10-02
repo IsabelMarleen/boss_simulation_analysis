f68440e5ce64610a4811ab04bfba7e697fbe6ec5
IsabelMarleen <52419857+IsabelMarleen@users.noreply.github.com>
2025-10-02 14:19:27 +0100

    Add configuration files for different experiments

--- workflow/config/otu_info_og.py
+++ workflow/config/otu_info_og.py
@@ -3,9 +3,11 @@
 # this translates the names as they appear in the downloaded fasta to cleaner names
 # and it also associates contigs from the same organism to the same name
 otus_clean_names = {
-    # 'chr21' : 'hschr21',
-    '21' : 'hschr21' #,
-    # 'ENA|CP068264|CP068264.2' : 'hschr14'
+    'BS.pilon.polished.v3.ST170922' : 'bsubtilis',
+    'Escherichia_coli_plasmid' : 'ecoli',
+    'Escherichia_coli_chromosome' : 'ecoli',
+    'Listeria_monocytogenes_complete_genome' : 'lmonocytogenes',
+    'Pseudomonas_aeruginosa_complete_genome' : 'paeruginosa',
 }
 
 # this is a dict that just contains the chromosomes, no plasmids
@@ -15,8 +17,8 @@ otus_no_plasmids = {k: v for k, v in otus_clean_names.items() if 'plasmid' not i
 
 # genome sizes used to calculate mean coverage
 genome_sizes = {
-    'hschr21': 45090682 #,
-    #'hschr14': 101161492,
+    'bsubtilis': 4045709,
+    'ecoli': 4765464,
+    'lmonocytogenes': 2992383,
+    'paeruginosa': 6792371,
 }
-
- 
